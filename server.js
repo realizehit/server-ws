9a512a68e78587da76bdb881f2b34f59d0cca2e9
José Moreira <jose.moreira@findhit.com>
2016-01-22 13:48:35 +0000

    Server: fix env

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var WSServer = require( './' )
 var env = process.env
 
 var REDIS_URI = env.REDIS_URI || 'redis://localhost:6379'
-var HTTP_PORT = env.SERVER_API_PORT || 8080
+var HTTP_PORT = env.SERVER_WS_PORT || 8080
 var HTTP_HOST = env.SERVER_HOST || '0.0.0.0'
-var ENDPOINT = env.ENDPOINT_API || 'http://' + HTTP_HOST + ':' + HTTP_PORT
+var ENDPOINT = env.ENDPOINT_WS || 'ws://' + HTTP_HOST + ':' + HTTP_PORT + '/ws'
 
 var server = new WSServer({
     httpPort: HTTP_PORT,
